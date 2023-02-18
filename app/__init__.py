f9bd82a1040aa7956f05ac133ed90a94a46555de
erossy <veltun@gmail.com>
2023-02-19 02:42:23 +0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,19 +9,19 @@ app = Flask(__name__)
 def helloworld():
     return "Hello World with Python Flask!"
 
-@app.route('/')
-def hello():
-    html = "<h3>Hello World from {hostname}!</h3>"
-    html += "<h3>Your random word is: {random_word}</h3>"
-    db = mysql.connector.connect(
-              host=os.getenv("MYSQL_SERVICE_HOST"),
-              port=os.getenv("MYSQL_SERVICE_PORT"),
-              user="root",
-              passwd=os.getenv("MYSQL_DB_PASSWORD"),
-              database="pyapplicationdb",
-              auth_plugin="mysql_native_password"
-         )
-    cursor = db.cursor()
-    cursor.execute("select word from random_words order by rand() limit 1;")
-    res = cursor.fetchall()
-    return html.format(random_word=res[0][0], hostname=socket.gethostname())
+#@app.route('/')
+#def hello():
+    #html = "<h3>Hello World from {hostname}!</h3>"
+    #html += "<h3>Your random word is: {random_word}</h3>"
+    #db = mysql.connector.connect(
+    #          host=os.getenv("MYSQL_SERVICE_HOST"),
+    #          port=os.getenv("MYSQL_SERVICE_PORT"),
+    #          user="root",
+    #          passwd=os.getenv("MYSQL_DB_PASSWORD"),
+    #          database="pyapplicationdb",
+    #          auth_plugin="mysql_native_password"
+    #     )
+    #cursor = db.cursor()
+    #cursor.execute("select word from random_words order by rand() limit 1;")
+    #res = cursor.fetchall()
+    #return html.format(random_word=res[0][0], hostname=socket.gethostname())
